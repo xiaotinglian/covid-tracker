ba29d445a4feab2378d3ebdff45106425b367654
Xiaoting Lian <lian1995226@gmail.com>
2022-05-10 14:15:40 -0400

    update

---
+++ src/main/java/com/example/covidtracker/Controller/MyController.java
@@ -0,0 +1,29 @@
+package com.example.covidtracker.Controller;
+
+import com.example.covidtracker.Models.LocationStats;
+import com.example.covidtracker.Services.CovidDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+    @Autowired
+    CovidDataService covidDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = covidDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+
+}
--- src/main/java/com/example/covidtracker/CovidTrackerApplication.java
+++ src/main/java/com/example/covidtracker/CovidTrackerApplication.java
@@ -2,12 +2,14 @@ package com.example.covidtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CovidTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CovidTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTrackerApplication.class, args);
+    }
 
 }
